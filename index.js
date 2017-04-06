b4342c3e2453553497408c3004105c4f249b6f92
bredele <olivier.wietrich@gmail.com>
2017-04-06 17:59:53 -0600

    set and get buffer json object

--- index.js
+++ index.js
@@ -5,14 +5,21 @@
 const extract = require('binary-extract')
 
 /**
+ * Extract values from json buffer and expose an
+ * object-like API.
  *
+ * @param {String||Buffer}
+ * @return {Object}
+ * @api public
  */
 
 module.exports = function(buffer) {
   buffer = new Buffer(buffer)
   return new Proxy({}, {
     get(target, key) {
-      return extract(buffer, key)
+      return key in target
+        ? target[key]
+        : extract(buffer, key)
     }
   })
 }
--- test/json-binary.js
+++ test/json-binary.js
@@ -7,6 +7,7 @@ const json = require('..')
 const read = require('fs').readFile
 const extract = require('binary-extract')
 
+
 test('should read value from json buffer', assert => {
   assert.plan(1)
   read(__dirname + '/test.json', (err, buffer) => {
@@ -19,4 +20,18 @@ test('should buffer json if passed as a string', assert => {
   assert.plan(1)
   const obj = json('{"name":"olivier"}')
   assert.equal(obj.name, 'olivier')
+})
+
+test('should set value', assert => {
+  assert.plan(1)
+  const obj = json('{"name":"olivier"}')
+  obj.name = 'klara'
+  assert.equal(obj.name, 'klara')
+})
+
+test('should set values', assert => {
+  assert.plan(1)
+  const obj = json('{"name":"olivier", "city":"san-francisco"}')
+  obj.city = 'calgary'
+  assert.equal(obj.city, 'calgary')
 })
